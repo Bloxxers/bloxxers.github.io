9ca75edfaeff62d22121f307d95c94c46bda8c69
BloxxersTeam <67830711+Bloxxers@users.noreply.github.com>
2020-07-04 20:23:58 +0100

    Add files via upload

---
+++ Scripts/Banners.js
@@ -0,0 +1,9 @@
+var Banners = [
+    "https://bloxxers.github.io/Bloxxers/Banners/banner.png"
+    , "https://bloxxers.github.io/Bloxxers/Banners/banner.png"
+    , "https://bloxxers.github.io/Bloxxers/Banners/banner.png"
+    ]
+var Banner = '';
+var BannerExample = Math.floor(Math.random() * Titles.length);
+Banner += Banners[BannerExample];
+return Banner
