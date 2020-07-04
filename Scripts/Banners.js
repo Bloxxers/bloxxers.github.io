14bf5096db2e6b6096b0b1dc7512d0d62a389df1
BloxxersTeam <67830711+Bloxxers@users.noreply.github.com>
2020-07-04 21:36:36 +0100

    Update Banners.js

--- Scripts/Banners.js
+++ Scripts/Banners.js
@@ -3,6 +3,7 @@ var Banners = new Array();
 Banners[0] = "https://bloxxers.github.io/Bloxxers/Banners/banner.png";
 Banners[1] = "https://bloxxers.github.io/Bloxxers/Banners/banner2.png";
 Banners[2] = "https://bloxxers.github.io/Bloxxers/Banners/banner3.png";
+Banners[3] = "https://bloxxers.github.io/Bloxxers/Banners/banner4.png";
 
 function Banner() {
   var Banner = Math.floor(Math.random()*Banners.length);
